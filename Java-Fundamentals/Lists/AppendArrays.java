7a198d44af1fd910e726b347294ddab3da6db710
StiuIvanov <stiu.ivanov@gmail.com>
2021-02-17 19:46:01 +0200

    Added Lists to Java-Fundamentals

---
+++ Java-Fundamentals/Lists/AppendArrays.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AppendArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] arrays = scanner.nextLine().split("\\|");
+        String result = "";
+        for (int i = arrays.length - 1; i >= 0; i--) {
+            String[] currentArr = arrays[i].split("\\s+");
+            for (int j = 0; j < currentArr.length ; j++) {
+                if (!currentArr[j].equals("")){
+                    result = result + currentArr[j]+ " ";
+                }
+
+            }
+        }
+        System.out.println(result);
+    }
+}
