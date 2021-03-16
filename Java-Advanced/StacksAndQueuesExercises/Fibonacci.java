2ec6baafd4bd520df87184a6fa49ffae257e3669
StiuIvanov <stiu.ivanov@gmail.com>
2021-03-17 00:03:14 +0200

    Add exercise to StacksAndQueuesExercises

---
+++ Java-Advanced/StacksAndQueuesExercises/Fibonacci.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static long[] memoryForFibonacci;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        memoryForFibonacci = new long[n + 1];
+        System.out.println(fibonacci(n));
+    }
+
+    private static long fibonacci(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        if (memoryForFibonacci[n] != 0) {
+            return memoryForFibonacci[n];
+        }
+        return memoryForFibonacci[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
