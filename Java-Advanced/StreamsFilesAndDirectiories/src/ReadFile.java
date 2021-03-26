25b5ec8a7725391791d66675e23c559b9ee6cb13
StiuIvanov <stiu.ivanov@gmail.com>
2021-03-27 01:11:36 +0200

    Add exercise in StreamsFilesAndDirectiories

---
+++ Java-Advanced/StreamsFilesAndDirectiories/src/ReadFile.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\Stiu\\Downloads\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+
+        FileInputStream inputStream = new FileInputStream(path);
+
+        int singleByte = inputStream.read();
+        while (singleByte >= 0) {
+
+            System.out.print(Integer.toBinaryString(singleByte)+ " ");
+
+            singleByte = inputStream.read();
+        }
+
+
+    }
+}
