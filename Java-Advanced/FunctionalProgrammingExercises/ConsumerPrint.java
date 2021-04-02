7d912e6575d80bb4fa1e336a6840990345996cde
StiuIvanov <stiu.ivanov@gmail.com>
2021-04-03 01:20:22 +0300

    Add exercise in FunctionalProgrammingExercises

---
+++ Java-Advanced/FunctionalProgrammingExercises/ConsumerPrint.java
@@ -0,0 +1,17 @@
+    import java.util.Arrays;
+    import java.util.Scanner;
+    import java.util.function.Consumer;
+
+    public class ConsumerPrint {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            Consumer<String> printString = s -> System.out.println(s);
+
+            Arrays.stream(scanner.nextLine().split("\\s+"))
+            .forEach(printString::accept);
+
+
+
+        }
+    }
