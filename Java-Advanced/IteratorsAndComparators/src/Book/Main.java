e1478909404d3760e4dedc6260e843fcfb3f2191
StiuIvanov <stiu.ivanov@gmail.com>
2021-04-24 20:53:05 +0300

    Add exercise in IteratorsAndComparators

---
+++ Java-Advanced/IteratorsAndComparators/src/Book/Main.java
@@ -0,0 +1,13 @@
+package Book;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Tales",1956, "John", "Drew");
+        Book book2 = new Book("Earth",1931, "Sam Smith", "Carl");
+
+
+
+
+    }
+}
