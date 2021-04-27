2d518e4bd6c823ebd5d3dc1b54d52adc012f6fe5
StiuIvanov <stiu.ivanov@gmail.com>
2021-04-28 01:56:20 +0300

    Add exercise in IteratorsAndComparatorExercises

---
+++ Java-Advanced/IteratorsAndComparatorExercises/src/ListyIterator/ListyIterator.java
@@ -0,0 +1,33 @@
+package ListyIterator;
+
+import java.util.List;
+
+public class ListyIterator {
+    private List<String> data;
+    private int index;
+
+    public ListyIterator(List<String> data) {
+        this.data = data;
+    }
+
+    public boolean move() {
+        if (hasNext()) {
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean hasNext() {
+        return index < data.size() - 1;
+    }
+
+    public void print() {
+        if (data.isEmpty()){
+            throw new IllegalStateException("Invalid Operation!");
+        }
+        System.out.println(data.get(index));
+    }
+
+
+}
