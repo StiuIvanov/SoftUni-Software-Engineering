a84f1407e620425febba985700388d05b9d6f1ab
StiuIvanov <stiu.ivanov@gmail.com>
2021-05-28 01:28:55 +0300

    Add exercise in InheritanceExercises

---
+++ Java-OOP/InheritanceExercises/src/restaurant/MainDish.java
@@ -0,0 +1,9 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class MainDish extends  Food{
+    public MainDish(String name, BigDecimal price, double gram) {
+        super(name, price, gram);
+    }
+}
