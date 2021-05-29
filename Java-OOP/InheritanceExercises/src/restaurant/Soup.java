51ec71456951de74d358cb1eb9b6b49f9505ecf1
StiuIvanov <stiu.ivanov@gmail.com>
2021-05-30 00:32:16 +0300

    Add exercise in InheritanceExercises

---
+++ Java-OOP/InheritanceExercises/src/restaurant/Soup.java
@@ -0,0 +1,9 @@
+package restaurant;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter{
+    public Soup(String name, BigDecimal price, double gram) {
+        super(name, price, gram);
+    }
+}
