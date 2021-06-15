f16cc04a90eb7955ffbe0ae6314130b59d438653
StiuIvanov <stiu.ivanov@gmail.com>
2021-06-15 20:34:07 +0300

    Add exercise in InterfacesAndAbstractionExercises

---
+++ Java-OOP/InterfacesAndAbstractionExercises/src/MilitaryElite/interfaces/Commando.java
@@ -0,0 +1,8 @@
+package MilitaryElite.interfaces;
+
+import java.util.Collection;
+
+public interface Commando extends Private {
+    void addMission(Mission mission);
+    Collection<Mission> getMissions();
+}
