12ed9d3a82b86a82193e0d6f3f6244caf02145e5
StiuIvanov <stiu.ivanov@gmail.com>
2021-06-15 20:34:34 +0300

    Add exercise in InterfacesAndAbstractionExercises

---
+++ Java-OOP/InterfacesAndAbstractionExercises/src/MilitaryElite/interfaces/Engineer.java
@@ -0,0 +1,10 @@
+package MilitaryElite.interfaces;
+
+import java.util.Collection;
+
+public interface Engineer extends Private {
+
+    void addRepair(Repair repair);
+
+    Collection<Repair> getRepairs();
+}
