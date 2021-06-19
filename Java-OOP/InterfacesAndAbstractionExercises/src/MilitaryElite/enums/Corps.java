89fd439895a3f48235c8f54068a84fce66711654
StiuIvanov <stiu.ivanov@gmail.com>
2021-06-19 09:24:23 +0300

    Add exercise in InterfacesAndAbstractionExercises

---
+++ Java-OOP/InterfacesAndAbstractionExercises/src/MilitaryElite/enums/Corps.java
@@ -0,0 +1,12 @@
+package MilitaryElite.enums;
+
+public enum Corps {
+    AIRFORCES("Airforces"),
+    MARINES("Marines");
+
+    private String value;
+
+    Corps(String value) {
+        this.value = value;
+    }
+}
