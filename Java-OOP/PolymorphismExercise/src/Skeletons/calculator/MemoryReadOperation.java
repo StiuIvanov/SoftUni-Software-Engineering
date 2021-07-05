ffd2658d531015f8d7be607fbad26c58021bd57e
StiuIvanov <stiu.ivanov@gmail.com>
2021-07-05 23:17:57 +0300

    Add exercise in PolymorphismExercise

---
+++ Java-OOP/PolymorphismExercise/src/Skeletons/calculator/MemoryReadOperation.java
@@ -0,0 +1,25 @@
+package Skeletons.calculator;
+
+import java.util.Deque;
+
+public class MemoryReadOperation implements Operation{
+    private Deque<Integer> memory;
+    public MemoryReadOperation(Deque<Integer> memory) {
+        this.memory=memory;
+    }
+
+    @Override
+    public void addOperand(int operand) {
+
+    }
+
+    @Override
+    public int getResult() {
+        return this.memory.pop();
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return memory.size() != 0;
+    }
+}
