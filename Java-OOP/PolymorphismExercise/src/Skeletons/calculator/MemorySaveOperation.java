7853c3535c37b810f027a71730833762ecdc91c1
StiuIvanov <stiu.ivanov@gmail.com>
2021-07-05 23:18:12 +0300

    Add exercise in PolymorphismExercise

---
+++ Java-OOP/PolymorphismExercise/src/Skeletons/calculator/MemorySaveOperation.java
@@ -0,0 +1,29 @@
+package Skeletons.calculator;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+public class MemorySaveOperation implements Operation{
+    private Deque<Integer> memory;
+
+     public MemorySaveOperation(Deque<Integer> memory){
+         this.memory=memory;
+     }
+
+
+    @Override
+    public void addOperand(int operand) {
+        this.memory.push(operand);
+    }
+
+    @Override
+    public int getResult() {
+        return this.memory.peek();
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return false;
+    }
+}
